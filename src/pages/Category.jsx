a67d1057dc193e71de7836f8fcc06de34309a1c6
thayanithi15-git <thayanithi2006s@gmail.com>
2024-09-26 11:32:21 +0530

    thaya page

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -12,7 +12,7 @@ import { BsDisplay } from "react-icons/bs";
 
 export default function Category() {
   const Categories = [
-    { name: "Puppy", image: Puppy },
+    { name: "Animal", image: Puppy },
     { name: "Cats", image: Cat },
     { name: "Fish", image: Fish },
     { name: "Birds", image: Bird },
